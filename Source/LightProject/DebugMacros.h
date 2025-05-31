9f742343d3c1af32526c895a3be52460ef7c0761
Tomas Linares <totoli2809@gmail.com>
2025-05-31 01:25:27 -0300

    Macros Debug Added
    Added shapes for debugs

---
+++ Source/LightProject/DebugMacros.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "DrawDebugHelpers.h"
+
+#define DRAW_SPHERE(Location) if(GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+#define DRAW_SPHERE(Location, Color) if(GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, Color, true);
+#define DRAW_LINE(StartLocation, EndLocation) if(GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
+#define DRAW_POINT(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Green, true);
+#define DRAW_VECTOR(StartLocation, EndLocation) if(GetWorld()) \
+{ \
+DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f); \
+DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Green, true); \
+}
--- Source/LightProject/Private/Items/Item.cpp
+++ Source/LightProject/Private/Items/Item.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Items/Item.h"
+#include "LightProject/DebugMacros.h"
 
 AItem::AItem()
 {
@@ -11,11 +12,20 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	FColor Color = FColor::Green;
+	
+	DRAW_SPHERE(Location, Color);
+	//DRAW_LINE(Location, Location + Forward * 100.f);
+	//DRAW_POINT(Location + Forward * 100.f);
+	DRAW_VECTOR(Location, Location + Forward * 100.f);
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
